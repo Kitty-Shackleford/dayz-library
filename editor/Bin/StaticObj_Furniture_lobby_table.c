166bc741ac29b1983919d4b7a04df768abb8bf95
Agent Buzz <agentbuzz@protonmail.com>
2024-09-25 23:30:59 -0500

    Summary

---
+++ editor/Bin/StaticObj_Furniture_lobby_table.c
@@ -0,0 +1,32 @@
+static Object SpawnObject(string type, vector position, vector orientation, float scale = 1.0)
+{
+    Object obj = GetGame().CreateObjectEx(type, position, ECE_SETUP | ECE_UPDATEPATHGRAPH | ECE_CREATEPHYSICS);
+    if (!obj) {
+        Error("Failed to create object " + type);
+        return null;
+    }
+
+    obj.SetPosition(position);
+    obj.SetOrientation(orientation);
+    obj.SetOrientation(obj.GetOrientation());
+    obj.SetScale(scale);
+    obj.Update();
+	obj.SetAffectPathgraph(true, false);
+	if (obj.CanAffectPathgraph()) {
+        GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(GetGame().UpdatePathgraphRegionByObject, 100, false, obj);
+    } 
+
+    return obj;
+}
+
+// Paste anything below this line into the bottom of your 'void main()' function
+
+
+// Created Objects
+SpawnObject("StaticObj_Furniture_lobby_table", "4638.830078 339.122986 10354.200195", "0.000000 0.000000 0.000000", 1);
+SpawnObject("M4A1", "4638.819824 339.661987 10354.113281", "179.999985 -89.832130 179.999985", 0.999998);
+
+
+// Uncomment if you want to export loot from newly added buildings
+// Position, Radius (increase if you have a larger map than Chernarus)
+// GetCEApi().ExportProxyData(Vector(7500, GetGame().SurfaceY(7500, 7500), 7500), 20000);
